c96190a4c04d7b391befdc3cbbaac29da9c96529
stillanewbie <boyu0107@gmail.com>
2019-09-16 20:58:31 -0600

    deckservice 1

--- BlackJack/src/main/java/service/GenericPlayerService.java
+++ BlackJack/src/main/java/service/GenericPlayerService.java
@@ -1,4 +1,82 @@
 package service;
 
+import model.Card;
+import model.GenericPlayer;
+
+import java.util.List;
+
 public abstract class GenericPlayerService {
+    private GenericPlayer player;
+
+    public GenericPlayerService(GenericPlayer player) {
+        this.player = player;
+    }
+
+    public GenericPlayer getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(GenericPlayer player) {
+        this.player = player;
+    }
+
+    public int getCumulativeScore() {
+        return this.player.getCumulativeScore();
+    }
+
+    public void setCumulativeScore(int value) {
+        this.player.setCumulativeScore(getCumulativeScore() + value);
+    }
+
+    public int getScore() {
+        return this.player.getScore();
+    }
+
+    public void setScore(int score) {
+        this.player.setScore(score);
+    }
+
+    public int getNumOfAce() {
+        return this.player.getNumOfAce();
+    }
+
+    public void setNumOfAce(int value) {
+        this.player.setNumOfAce(value);
+    }
+
+    public List<Card> getCardsOnBoard() {
+        return this.player.getCardsOnBoard();
+    }
+
+    public void setCardsOnBoard(List<Card> cards) {
+        this.player.setCardsOnBoard(cards);
+    }
+
+    public void clearCumulativeScore() {
+        setCumulativeScore(0);
+    }
+
+    public void clearNumOfAce() {
+        setNumOfAce(0);
+    }
+
+    public void clearScore() {
+        setScore(0);
+    }
+
+    public String hit(Card card) {
+
+
+        return null;
+    }
+
+    String validateBeforeHit() {
+
+        return null;
+    }
+
+    String validateAfterhit() {
+
+        return null;
+    }
 }
