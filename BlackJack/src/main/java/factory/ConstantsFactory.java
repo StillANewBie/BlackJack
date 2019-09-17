4c6b2f9563941e785f85bc517f15074386ab2f9b
stillanewbie <boyu0107@gmail.com>
2019-09-16 20:48:45 -0600

    factories

---
+++ BlackJack/src/main/java/factory/ConstantsFactory.java
@@ -0,0 +1,8 @@
+package factory;
+
+public class ConstantsFactory {
+    public static final int GAME_GOAL = 21;
+    public static final int SHUFFLE_TIMES = 10;
+    public static final int ACE_HIGH_VALUE = 11;
+    public static final int NUM_DECK = 1;
+}
--- BlackJack/src/main/java/service/GameService.java
+++ BlackJack/src/main/java/service/GameService.java
@@ -1,4 +1,15 @@
 package service;
 
+import model.Card;
+import model.Dealer;
+import model.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameService {
+
+    private List<Card> cards;
+    private DealerService dealer;
+    private List<PlayerService> players;
 }
