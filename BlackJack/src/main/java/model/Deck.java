3567e44888b15cc26d1eed34b91f9d29556b8949
stillanewbie <boyu0107@gmail.com>
2019-09-16 20:32:16 -0600

    deck

--- BlackJack/src/main/java/model/Deck.java
+++ BlackJack/src/main/java/model/Deck.java
@@ -1,4 +1,30 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Deck {
+    private List<Card> cards;
+
+    public Deck() {
+        this.cards = new ArrayList<Card>();
+        for (Suit suit : Suit.values()) {
+            for (Rank rank : Rank.values()) {
+                this.cards.add(new Card(suit, rank));
+            }
+        }
+    }
+
+    public void setCards(List<Card> cards) {
+        this.cards = cards;
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public static void main(String[] args) {
+        Deck deck = new Deck();
+        deck.getCards().forEach((el) -> System.out.println(el));
+    }
 }
